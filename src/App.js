679fdbbc47e4e2f852398fa71e4082af49078f96
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-01-14 17:40:58 -0300

    .

--- src/App.js
+++ src/App.js
@@ -7,40 +7,40 @@ function App() {
 
   const times = [
     {
-      nome:'Programação',
-      corPrimaria:'#12365',
-      corSecundaria:'#652434'
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
     },
     {
-      nome:'Programação',
-      corPrimaria:'',
-      corSecundaria:''
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
     },
     {
-      nome:'Programação',
-      corPrimaria:'',
-      corSecundaria:''
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
     },
     {
-      nome:'Programação',
-      corPrimaria:'',
-      corSecundaria:''
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
     },
     {
-      nome:'Programação',
-      corPrimaria:'',
-      corSecundaria:''
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
     },
     {
-      nome:'Programação',
-      corPrimaria:'',
-      corSecundaria:''
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
     },
     {
-      nome:'Programação',
-      corPrimaria:'',
-      corSecundaria:''
-    },
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
   ]
 
   const [colaboradores, setColaboradores] = useState([])
@@ -53,9 +53,15 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      
-      {times.map (time => <Time key={time.nome} nome={time.nome} corDoFundo={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
 
     </div>
   );
